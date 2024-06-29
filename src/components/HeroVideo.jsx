3ce72f56e52a1b97715c3c9dc6b32b67c67015e7
vikas <121391039+Vikas-rawat1@users.noreply.github.com>
2024-06-30 04:45:21 +0530

    Update HeroVideo.jsx

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -13,7 +13,7 @@ function HeroVideo() {
         ></video>
         <div className="absolute text-center top-60 md:top-44 md:text-start md:w-4/5 md:ml-20 text-white">
           <h1 className="text-5xl md:text-7xl font-bold mb-8">
-            Watch the 28th Annual Webby Awards
+            Watch The 28th Annual Webby Awards
           </h1>
           <button className="text-xl font-semibold text-black bg-white rounded-full p-3 w-3/5 md:w-2/5  lg:w-1/5">
             Highlights Here
