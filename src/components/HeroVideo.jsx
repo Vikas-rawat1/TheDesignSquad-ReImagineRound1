8b7598777d32df49d99b744310bde1e1a275da89
vikas <rawatvikas689@gmail.com>
2024-06-30 17:31:59 +0530

    commit

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -12,7 +12,7 @@ function HeroVideo() {
           className="absolute inset-0 w-full h-full object-cover"
         ></video>
         <div className="absolute text-center top-60 md:top-44 md:text-start md:w-4/5 md:ml-20 text-white">
-          <h1 className="text-5xl md:text-7xl font-bold mb-8">
+          <h1 className="text-5xl md:text-7xl font-bold mb-8 lg:w-4/5">
             Watch The 28th Annual Webby Awards
           </h1>
           <button className="text-xl font-semibold text-black bg-white rounded-full p-3 w-3/5 md:w-2/5  lg:w-1/5">
