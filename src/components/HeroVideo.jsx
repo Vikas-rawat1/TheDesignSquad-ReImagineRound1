cd33719a8e23b3ee3fd99e3324eb308f44e16058
vikas <rawatvikas689@gmail.com>
2024-06-30 17:43:02 +0530

    commit

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -2,25 +2,27 @@ import React from "react";
 
 function HeroVideo() {
   return (
-    <>
-      <div className="relative w-full h-screen overflow-hidden">
-        <video
-          src="videos/hero.webm"
-          autoPlay
-          loop
-          muted
-          className="absolute inset-0 w-full h-full object-cover"
-        ></video>
-        <div className="absolute text-center top-60 md:top-44 md:text-start md:w-4/5 md:ml-20 text-white">
-          <h1 className="text-5xl md:text-7xl font-bold mb-8 lg:w-4/5">
-            Watch The 28th Annual Webby Awards
-          </h1>
-          <button className="text-xl font-semibold text-black bg-white rounded-full p-3 w-3/5 md:w-2/5  lg:w-1/5">
-            Highlights Here
-          </button>
-        </div>
+    <div className="relative w-full h-screen overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        preload="metadata"
+        className="absolute inset-0 w-full h-full object-cover"
+      >
+        <source src="videos/hero.webm" type="video/webm" />
+        <source src="videos/hero.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="absolute text-center top-60 md:top-44 md:text-start md:w-4/5 md:ml-20 text-white">
+        <h1 className="text-5xl md:text-7xl font-bold mb-8 lg:w-4/5">
+          Watch The 28th Annual Webby Awards
+        </h1>
+        <button className="text-xl font-semibold text-black bg-white rounded-full p-3 w-3/5 md:w-2/5 lg:w-1/5">
+          Highlights Here
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
