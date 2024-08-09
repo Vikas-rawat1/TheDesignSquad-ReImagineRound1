42e9e4ff846efe00e6f2cf51ccc67b795c689afa
vikas <rawatvikas689@gmail.com>
2024-08-09 16:03:40 +0530

    commit

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -88,7 +88,7 @@ function Achievements() {
           768: {
             slidesPerView: 2,
           },
-          1024: {
+          1024: {sdf
             slidesPerView: 4,
           },
         }}wwewe
